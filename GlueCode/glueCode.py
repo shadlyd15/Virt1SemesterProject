f636c4ad96d51cd0c7b757f0fbe7400cbe50bb59
shadlyd15 <shadlyd15@gmail.com>
2020-01-21 07:27:52 +0100

    Readme Changed

--- GlueCode/glueCode.py
+++ GlueCode/glueCode.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-# Copyright (c) 2010-2013 Roger Light <roger@atchoo.org>
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Distribution License v1.0
-# which accompanies this distribution.
-#
-# The Eclipse Distribution License is available at
-#   http://www.eclipse.org/org/documents/edl-v10.php.
-#
-# Contributors:
-#    Roger Light - initial implementation
-# Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
-# All rights reserved.
-
-# This shows a simple example of an MQTT subscriber.
-
-# import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.client as mqtt
 import pymongo as PyMongo
+
 from datetime import datetime
 
-dbClient = PyMongo.MongoClient('localhost', 27017);
+pyMongoClient = PyMongo.MongoClient('localhost', 27017);
 
-db = dbClient.prime_db_test
-primes = db.primes
+dbClient = pyMongoClient.prime_db_test
+primes = dbClient.primes
 
 def print_primes():
 	count = 0
@@ -35,26 +18,23 @@ def print_primes():
 			count += 1
 	print("Total " + str(count) + " Entries Found in Database\r\n")
 
-def on_connect(mqttc, obj, flags, rc):
+def on_connect(mqttClient, obj, flags, rc):
     print("MQTT Client Connected")
 
-def on_message(mqttc, obj, msg):
-	# nonlocal primes
+def on_message(mqttClient, obj, msg):
 	print("New Prime Received")
 	print(msg.topic + " : " + str(msg.payload) + " TimeStamp : " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 	result = primes.insert_one({'TPrime':msg.payload, 'TimeStamp':datetime.now().strftime("%m/%d/%Y, %H:%M:%S")})
 	print('Inserted Id : {0}'.format(result.inserted_id))
-	# print_primes()
 
-def on_subscribe(mqttc, obj, mid, granted_qos):
+def on_subscribe(mqttClient, obj, mid, granted_qos):
 	print("Subscribed")
 
-mqttc = mqtt.Client()
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
+mqttClient = mqtt.Client()
+mqttClient.on_message = on_message
+mqttClient.on_connect = on_connect
 
-mqttc.connect("localhost", 1883, 60)
-mqttc.subscribe("Prime", 0)
+mqttClient.connect("localhost", 1883, 60)
+mqttClient.subscribe("Prime", 0)
 
-
-mqttc.loop_forever()
+mqttClient.loop_forever()
--- PrimeLauncher/primeLauncher.py
+++ PrimeLauncher/primeLauncher.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-# Copyright (c) 2010-2013 Roger Light <roger@atchoo.org>
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Distribution License v1.0
-# which accompanies this distribution.
-#
-# The Eclipse Distribution License is available at
-#   http://www.eclipse.org/org/documents/edl-v10.php.
-#
-# Contributors:
-#    Roger Light - initial implementation
-# Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
-# All rights reserved.
-
-# This shows a simple example of an MQTT subscriber.
-
-# import context  # Ensures paho is in PYTHONPATH
-import random
 import paho.mqtt.client as mqtt
 import pymongo as PyMongo
 
-client = PyMongo.MongoClient('localhost', 27017);
+pyMongoClient = PyMongo.MongoClient('localhost', 27017);
 
-db = client.prime_db_test
-primes = db.primes
+dbClient = pyMongoClient.prime_db_test
+primes = dbClient.primes
 
 def print_primes():
 	count = 0
